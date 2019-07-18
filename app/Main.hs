9f66cb9c053c2da54eaaf8b7f9dc5d17db976edb
1inguini <9647142@gmail.com>
2019-07-18 23:53:04 +0900

    Sexyがちゃんと動くようになったのでTreeは用済み。消しました。

--- app/Main.hs
+++ app/Main.hs
@@ -1,71 +1,10 @@
 module Main where
 
 -- import           Lib
-import           Control.Monad
-import           Data.List
 import           System.Environment
 import           Text.Parsec
 import           Text.ParserCombinators.Parsec
 import           Text.Pretty.Simple
-
-listToString :: String -> [String] -> String
-listToString seperator =
-  foldl1 (\theWords word -> theWords ++  seperator ++ word)
-
-data SVal = Expr    Expr
-          | Integer Integer
-          | Bool    Bool
-          | List    Tree
-          | SValError String
-
-
-sVal :: String -> SVal
-sVal "+" = Expr Add
-sVal "-" = Expr Sub
-sVal "*" = Expr Times
-sVal "/" = Expr Div
-sVal "%" = Expr Mod
-sVal "t" = Bool True
-sVal "f" = Bool False
-sVal str = case parse (many1 digit) "" str of
-  Right num -> Integer (read num)
-  _         -> SValError ("'" ++ str ++ "' is not valid character")
-
-showSVal :: SVal -> String
-showSVal (Expr Add)        = "add"
-showSVal (Expr Sub)        = "sub"
-showSVal (Expr Times)      = "times"
-showSVal (Expr Div)        = "div"
-showSVal (Expr Mod)        = "mod"
-showSVal (Integer integer) = show integer
-showSVal (Bool True)       = "true"
-showSVal (Bool False)      = "false"
-showSVal (List tree)       = show tree
-showSVal (SValError str)   = "SValError: " ++ (init . tail) (show str)
-
-data Expr = Add
-          | Sub
-          | Times
-          | Div
-          | Mod
-
-data Tree = Node [Tree]
-          | Leaf SVal
-
-instance Show SVal where show = showSVal
-instance Show Tree where show = showTree
-
-showTree :: Tree -> String
-showTree (Leaf sval)  = showSVal sval
-showTree (Node trees) =
-  "< " ++ listToString " " (map showTree trees) ++ " >"
-
-parseTree :: Parser Tree
-parseTree = spaces >> (node <|> parsed)
-  where
-    node = Node <$> between (char '(') (char ')') (many1 parseTree)
-    leaf = many1 (oneOf "+-*/%" <|> digit) <|> sepBy1 space (many (noneOf "()"))
-    parsed = Leaf . sVal <$> leaf
 
 data Sexy = NonNorm  (SexyFunc, Sexy, Sexy)
           | Norm     Types
