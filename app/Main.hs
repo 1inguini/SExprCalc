9486635deeac96c22da5d0568eb55dcd8eec492c
1inguini <9647142@gmail.com>
2019-07-19 01:02:01 +0900

    評価機が動いた。実績解除:インタプリタを自作!

--- app/Main.hs
+++ app/Main.hs
@@ -9,10 +9,41 @@ import           Text.Pretty.Simple
 data Sexy = NonNorm  (SexyFunc, Sexy, Sexy)
           | Norm     Types
 
-data SexyFunc = Arthmetic String -- (Integer -> Integer -> Integer)
+data SexyFunc = Arthmetic String -- (Sexy -> Sexy -> Sexy) -- (Integer -> Integer -> Integer)
+              | UnknownFunc
 
 data Types = SexyInteger Integer
+           | UnknownType
+           | TypeError   String
            -- | SexyBool    Bool
+
+sexyAdd :: Sexy -> Sexy -> Sexy
+sexyAdd (Norm (SexyInteger val1)) (Norm (SexyInteger val2)) =
+  (Norm . SexyInteger) (val1 + val2)
+sexyAdd _ _ = Norm UnknownType
+
+sexySub :: Sexy -> Sexy -> Sexy
+sexySub (Norm (SexyInteger val1)) (Norm (SexyInteger val2)) =
+  (Norm . SexyInteger) (val1 - val2)
+sexySub _ _ = Norm UnknownType
+
+sexyMulti :: Sexy -> Sexy -> Sexy
+sexyMulti (Norm (SexyInteger val1)) (Norm (SexyInteger val2)) =
+  (Norm . SexyInteger) (val1 * val2)
+sexyMulti _ _ = Norm UnknownType
+
+sexyDiv :: Sexy -> Sexy -> Sexy
+sexyDiv (Norm (SexyInteger val1)) (Norm (SexyInteger val2)) =
+  (Norm . SexyInteger) (div val1 val2)
+sexyDiv _ _ = Norm UnknownType
+
+sexyMod :: Sexy -> Sexy -> Sexy
+sexyMod (Norm (SexyInteger val1)) (Norm (SexyInteger val2)) =
+  (Norm . SexyInteger) (mod val1 val2)
+sexyMod _ _ = Norm UnknownType
+
+sexyUnkownFunc :: Sexy -> Sexy -> Sexy
+sexyUnkownFunc _ _ = Norm UnknownType
 
 showSexyFunc :: SexyFunc -> String
 showSexyFunc (Arthmetic "+") = "(Function: addition)"
@@ -20,27 +51,28 @@ showSexyFunc (Arthmetic "-") = "(Function: subtraction)"
 showSexyFunc (Arthmetic "*") = "(Function: mutiplication)"
 showSexyFunc (Arthmetic "/") = "(Function: division)"
 showSexyFunc (Arthmetic "%") = "(Function: modulo)"
-showSexyFunc (Arthmetic val) = "(Error: unknown function " ++ val ++ " )"
+showSexyFunc UnknownFunc     = "(Error: unknown)"
+showSexyFunc _               = "(Error: unknown function)"
+
 instance Show SexyFunc where show = showSexyFunc
 
 showSexy :: Sexy -> String
 showSexy (NonNorm (sexyfunc, sexy1, sexy2)) =
   showSexyFunc sexyfunc ++ " (Sexy: " ++ showSexy sexy1 ++ ") (Sexy: " ++ showSexy sexy2 ++ ")"
 showSexy (Norm types) = showTypes types
+
 instance Show Sexy where show = showSexy
 
 showTypes :: Types -> String
 showTypes (SexyInteger integer) = "(SexyInteger: " ++ show integer ++ ")"
+showTypes UnknownType           = "(UnknownType)"
+showTypes (TypeError err)       = "(TypeError:" ++ err ++ ")"
+
 instance Show Types where show = showTypes
 
 parseSexy :: Parser Sexy
 parseSexy = -- spaces >> (node <|> parsed)
   spaces >> (parseSexyNonNorm <|> parseSexyNorm)
-
-parseSexyNorm :: Parser Sexy
-parseSexyNorm = do
-  atom <- many1 (digit <|> letter)
-  return $ Norm (SexyInteger (read atom))
 
 parseSexyNonNorm :: Parser Sexy
 parseSexyNonNorm = do
@@ -52,10 +84,42 @@ parseSexyNonNorm = do
     ((Text.ParserCombinators.Parsec.try space) <|> (char ')'))
   return $ NonNorm (Arthmetic func, sexy1, sexy2)
 
+-- matchSexyFunc :: String -> SexyFunc
+-- matchSexyFunc "+" = (Arthmetic sexyAdd)
+-- matchSexyFunc "-" = (Arthmetic sexySub)
+-- matchSexyFunc "*" = (Arthmetic sexyMulti)
+-- matchSexyFunc "/" = (Arthmetic sexyDiv)
+-- matchSexyFunc "%" = (Arthmetic sexyMod)
+-- matchSexyFunc _   =  UnknownFunc
+
+parseSexyNorm :: Parser Sexy
+parseSexyNorm = do
+  atom <- many1 (digit <|> letter)
+  return $ Norm (SexyInteger (read atom))
+
+
+evalSexyFunc :: SexyFunc -> (Sexy -> Sexy -> Sexy)
+evalSexyFunc (Arthmetic "+") = sexyAdd
+evalSexyFunc (Arthmetic "-") = sexySub
+evalSexyFunc (Arthmetic "*") = sexyMulti
+evalSexyFunc (Arthmetic "/") = sexyDiv
+evalSexyFunc (Arthmetic "%") = sexyMod
+evalSexyFunc _               = sexyUnkownFunc
+
+evalSexy :: Sexy -> Sexy
+evalSexy sexy =
+  case sexy of
+    NonNorm (sexyFunc, sexy1, sexy2) -> (evalSexyFunc sexyFunc) (evalSexy sexy1) (evalSexy sexy2)
+    Norm  sexyInteger                -> Norm  sexyInteger
+
 main :: IO ()
 main = do
           args <- getArgs
           print $ head args
-          pPrint (parse parseSexy "" (head args))
+          pPrint
+            $ evalSexy
+            $ case (parse parseSexy "" (head args)) of
+                Right sexy -> sexy
+                Left err   -> Norm (TypeError (show err))
           parseTest parseSexy (head args)
           -- print (listToken (head args))
