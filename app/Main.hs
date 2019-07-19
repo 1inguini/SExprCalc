e009d32600ba8b1df05a44acbe3f4e9b5d628cac
1inguini <9647142@gmail.com>
2019-07-19 14:54:16 +0900

    Haskelineでreplの操作感を向上させるとともに「https://ja.wikibooks.org/wiki/48時間でSchemeを書こう」を参考にArthmetic型をArthmetic StringからArthmetic (String, Sexy -> Sexy -> Sexy)に変更

--- app/Main.hs
+++ app/Main.hs
@@ -1,75 +1,80 @@
 module Main where
 
 -- import           Lib
-import           System.Environment
+import           System.Console.Haskeline
+-- import           System.Environment
 import           System.IO
 import           Text.Parsec
 import           Text.ParserCombinators.Parsec
-import           Text.Pretty.Simple
+-- import           Text.Pretty.Simple
 
 data Sexy = NonNorm  (SexyFunc, Sexy, Sexy)
-          | Norm     Types
+          | Norm     Value
 
-data SexyFunc = Arthmetic String -- (Sexy -> Sexy -> Sexy) -- (Integer -> Integer -> Integer)
-              | UnknownFunc
+data SexyFunc = Arthmetic (String, (Sexy -> Sexy -> Sexy)) -- (Sexy -> Sexy -> Sexy) -- (Integer -> Integer -> Integer)
+              | UnknownFunc String
 
-data Types = SexyInteger Integer
-           | UnknownType
-           | TypeError   String
+data Value = SexyInteger Integer
+           | SexyError   String
            -- | SexyBool    Bool
 
 sexyAdd :: Sexy -> Sexy -> Sexy
 sexyAdd (Norm (SexyInteger val1)) (Norm (SexyInteger val2)) =
   (Norm . SexyInteger) (val1 + val2)
-sexyAdd _ _ = Norm UnknownType
+sexyAdd val1 val2 =
+  (Norm . SexyError) $ "(Error: Failed at addition of " ++ show val1 ++ " and " ++ show val2 ++ ".)"
 
 sexySub :: Sexy -> Sexy -> Sexy
-sexySub (Norm (SexyInteger val1)) (Norm (SexyInteger val2)) =
-  (Norm . SexyInteger) (val1 - val2)
-sexySub _ _ = Norm UnknownType
+sexySub (Norm (SexyInteger arg0)) (Norm (SexyInteger arg1)) =
+  (Norm . SexyInteger) (arg0 - arg1)
+sexySub arg0 arg1 =
+  (Norm . SexyError) $ "(Error: Failed at subtraction of " ++ show arg0 ++ " by " ++ show arg1 ++ ".)"
 
 sexyMulti :: Sexy -> Sexy -> Sexy
-sexyMulti (Norm (SexyInteger val1)) (Norm (SexyInteger val2)) =
-  (Norm . SexyInteger) (val1 * val2)
-sexyMulti _ _ = Norm UnknownType
+sexyMulti (Norm (SexyInteger arg0)) (Norm (SexyInteger arg1)) =
+  (Norm . SexyInteger) (arg0 * arg1)
+sexyMulti arg0 arg1 =
+  (Norm . SexyError) $ "(Error: Failed at muliplication of " ++ show arg0 ++ " and " ++ show arg1 ++ ".)"
 
 sexyDiv :: Sexy -> Sexy -> Sexy
-sexyDiv (Norm (SexyInteger val1)) (Norm (SexyInteger val2)) =
-  (Norm . SexyInteger) (div val1 val2)
-sexyDiv _ _ = Norm UnknownType
+sexyDiv (Norm (SexyInteger arg0)) (Norm (SexyInteger arg1)) =
+  (Norm . SexyInteger) (div arg0 arg1)
+sexyDiv arg0 arg1 =
+  (Norm . SexyError) $ "(Error: Failed at division of " ++ show arg0 ++ " by " ++ show arg1 ++ ".)"
 
 sexyMod :: Sexy -> Sexy -> Sexy
-sexyMod (Norm (SexyInteger val1)) (Norm (SexyInteger val2)) =
-  (Norm . SexyInteger) (mod val1 val2)
-sexyMod _ _ = Norm UnknownType
+sexyMod (Norm (SexyInteger arg0)) (Norm (SexyInteger arg1)) =
+  (Norm . SexyInteger) (mod arg0 arg1)
+sexyMod arg0 arg1 =
+  (Norm . SexyError) $ "(Error: Failed to yield the remainder from division of " ++ show arg0 ++ " by " ++ show arg1 ++ ".)"
 
 sexyUnkownFunc :: Sexy -> Sexy -> Sexy
-sexyUnkownFunc _ _ = Norm UnknownType
+sexyUnkownFunc arg0 arg1 =
+  (Norm . SexyError) $ "(Error: Unknown function applied to " ++ show arg0 ++ " and " ++ show arg1 ++ ".)"
 
 showSexyFunc :: SexyFunc -> String
-showSexyFunc (Arthmetic "+") = "(Function: addition)"
-showSexyFunc (Arthmetic "-") = "(Function: subtraction)"
-showSexyFunc (Arthmetic "*") = "(Function: mutiplication)"
-showSexyFunc (Arthmetic "/") = "(Function: division)"
-showSexyFunc (Arthmetic "%") = "(Function: modulo)"
-showSexyFunc UnknownFunc     = "(Error: unknown)"
-showSexyFunc _               = "(Error: unknown function)"
+showSexyFunc (Arthmetic ("+", _)) = "(Arthmetic: addition)"
+showSexyFunc (Arthmetic ("-", _)) = "(Arthmetic: subtraction)"
+showSexyFunc (Arthmetic ("*", _)) = "(Arthmetic: mutiplication)"
+showSexyFunc (Arthmetic ("/", _)) = "(Arthmetic: division)"
+showSexyFunc (Arthmetic ("%", _)) = "(Arthmetic: modulo)"
+showSexyFunc (Arthmetic (val, _)) = "(Unkown Arthmetic: " ++ val ++ ")"
+showSexyFunc (UnknownFunc func)   = "(Unknown function: " ++ func ++ ")"
 
 instance Show SexyFunc where show = showSexyFunc
 
 showSexy :: Sexy -> String
 showSexy (NonNorm (sexyfunc, sexy1, sexy2)) =
   showSexyFunc sexyfunc ++ " (Sexy: " ++ showSexy sexy1 ++ ") (Sexy: " ++ showSexy sexy2 ++ ")"
-showSexy (Norm types) = showTypes types
+showSexy (Norm types) = showValue types
 
 instance Show Sexy where show = showSexy
 
-showTypes :: Types -> String
-showTypes (SexyInteger integer) = "(SexyInteger: " ++ show integer ++ ")"
-showTypes UnknownType           = "(UnknownType)"
-showTypes (TypeError err)       = "(TypeError:" ++ err ++ ")"
+showValue :: Value -> String
+showValue (SexyInteger integer) = "(SexyInteger: " ++ show integer ++ ")"
+showValue (SexyError err)       = "(SexyError: " ++ err ++ ")"
 
-instance Show Types where show = showTypes
+instance Show Value where show = showValue
 
 parseSexy :: Parser Sexy
 parseSexy = -- spaces >> (node <|> parsed)
@@ -78,59 +83,63 @@ parseSexy = -- spaces >> (node <|> parsed)
 parseSexyNonNorm :: Parser Sexy
 parseSexyNonNorm = do
   _ <- char '('
-  func  <- spaces >> manyTill anyToken (Text.ParserCombinators.Parsec.try space)
+  funcKey <- spaces >> manyTill anyToken (Text.ParserCombinators.Parsec.try space)
   sexy1:_ <- spaces >> manyTill parseSexy
     ((Text.ParserCombinators.Parsec.try space) <|> (Text.ParserCombinators.Parsec.try (char ')')))
   sexy2:[] <- spaces >> manyTill parseSexy
     ((Text.ParserCombinators.Parsec.try space) <|> (char ')'))
-  return $ NonNorm (Arthmetic func, sexy1, sexy2)
 
--- matchSexyFunc :: String -> SexyFunc
--- matchSexyFunc "+" = (Arthmetic sexyAdd)
--- matchSexyFunc "-" = (Arthmetic sexySub)
--- matchSexyFunc "*" = (Arthmetic sexyMulti)
--- matchSexyFunc "/" = (Arthmetic sexyDiv)
--- matchSexyFunc "%" = (Arthmetic sexyMod)
--- matchSexyFunc _   =  UnknownFunc
+  let sexyFunc = case lookup funcKey sexyFuncs of
+                  Just funcKeyhole -> (funcKey, funcKeyhole)
+                  Nothing          -> (funcKey, sexyUnkownFunc)
+  return $ NonNorm (Arthmetic sexyFunc, sexy1, sexy2)
 
 parseSexyNorm :: Parser Sexy
 parseSexyNorm = do
   atom <- many1 (digit <|> letter)
+
   return $ Norm (SexyInteger (read atom))
 
+sexyFuncs :: [(String, Sexy -> Sexy -> Sexy)]
+sexyFuncs = [("+", sexyAdd),
+             ("-", sexySub),
+             ("*", sexyMulti),
+             ("/", sexyDiv),
+             ("%", sexyMod)]
 
 evalSexyFunc :: SexyFunc -> (Sexy -> Sexy -> Sexy)
-evalSexyFunc (Arthmetic "+") = sexyAdd
-evalSexyFunc (Arthmetic "-") = sexySub
-evalSexyFunc (Arthmetic "*") = sexyMulti
-evalSexyFunc (Arthmetic "/") = sexyDiv
-evalSexyFunc (Arthmetic "%") = sexyMod
-evalSexyFunc _               = sexyUnkownFunc
+evalSexyFunc (Arthmetic (_, sexyFunc)) = sexyFunc
+evalSexyFunc (UnknownFunc _)           = sexyUnkownFunc
 
 evalSexy :: Sexy -> Sexy
 evalSexy sexy =
   case sexy of
     NonNorm (sexyFunc, sexy1, sexy2) -> (evalSexyFunc sexyFunc) (evalSexy sexy1) (evalSexy sexy2)
-    Norm  sexyInteger                -> Norm  sexyInteger
+    val@(Norm (SexyInteger _))       -> val
+    val@(Norm (SexyError _))         -> val
 
 
 flushStr :: String -> IO ()
 flushStr str = putStr str >> hFlush stdout
 
-repl :: IO ()
+repl :: InputT IO ()
 repl = do
-  flushStr "SExprCalc >>> "
-  input <- getLine
-  flushStr $ show
+  maybeInput <- getInputLine "(SexyEval: )>>> "
+  let input = case maybeInput of
+       Just theInput -> theInput
+       Nothing       -> "Error: failed getting input" in
+    outputStrLn $ show
     $ evalSexy
     $ case (parse parseSexy "" input) of
     Right sexy -> sexy
-    Left err   -> Norm (TypeError (show err))
-  flushStr "\n"
+    Left err   -> (Norm . SexyError) (show err)
   repl
+
+
 
 main :: IO ()
 main = do
+  runInputT defaultSettings repl
 --           args <- getArgs
 --           putStrLn $ head args
 --           pPrint (parse parseSexy "" (head args))
@@ -140,4 +149,3 @@ main = do
 --                 Right sexy -> sexy
 --                 Left err   -> Norm (TypeError (show err))
 --           -- print (listToken (head args))
-  repl
