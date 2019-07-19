6e4a12d6e1bdb67c7ac75b896435949d865e2f31
1inguini <9647142@gmail.com>
2019-07-19 16:07:13 +0900

    All "import" is "import qualified" now

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,15 @@
 module Main where
 
 -- import           Lib
-import           System.Console.Haskeline
--- import           System.Environment
-import           System.IO
-import           Text.Parsec
-import           Text.ParserCombinators.Parsec
 -- import           Text.Pretty.Simple
+-- import           System.Environment
+-- import qualified System.IO                     as IO
+import qualified Data.Either                   as E
+import qualified Data.Maybe                    as M
+import qualified System.Console.Haskeline      as HLine
+import qualified Text.Parsec                   as P
+import qualified Text.ParserCombinators.Parsec as PCP
+
 
 data Sexy = NonNorm  (SexyFunc, Sexy, Sexy)
           | Norm     Value
@@ -18,39 +21,46 @@ data Value = SexyInteger Integer
            | SexyError   String
            -- | SexyBool    Bool
 
+sexyFuncs :: [(String, Sexy -> Sexy -> Sexy)]
+sexyFuncs = [("+", sexyAdd),
+             ("-", sexySub),
+             ("*", sexyMulti),
+             ("/", sexyDiv),
+             ("%", sexyMod)]
+
 sexyAdd :: Sexy -> Sexy -> Sexy
 sexyAdd (Norm (SexyInteger val1)) (Norm (SexyInteger val2)) =
   (Norm . SexyInteger) (val1 + val2)
 sexyAdd val1 val2 =
-  (Norm . SexyError) $ "(Error: Failed at addition of " ++ show val1 ++ " and " ++ show val2 ++ ".)"
+  (Norm . SexyError) $ "(Error: failed at addition of " ++ show val1 ++ " and " ++ show val2 ++ ")"
 
 sexySub :: Sexy -> Sexy -> Sexy
 sexySub (Norm (SexyInteger arg0)) (Norm (SexyInteger arg1)) =
   (Norm . SexyInteger) (arg0 - arg1)
 sexySub arg0 arg1 =
-  (Norm . SexyError) $ "(Error: Failed at subtraction of " ++ show arg0 ++ " by " ++ show arg1 ++ ".)"
+  (Norm . SexyError) $ "(Error: failed at subtraction of " ++ show arg0 ++ " by " ++ show arg1 ++ ")"
 
 sexyMulti :: Sexy -> Sexy -> Sexy
 sexyMulti (Norm (SexyInteger arg0)) (Norm (SexyInteger arg1)) =
   (Norm . SexyInteger) (arg0 * arg1)
 sexyMulti arg0 arg1 =
-  (Norm . SexyError) $ "(Error: Failed at muliplication of " ++ show arg0 ++ " and " ++ show arg1 ++ ".)"
+  (Norm . SexyError) $ "(Error: failed at muliplication of " ++ show arg0 ++ " and " ++ show arg1 ++ ")"
 
 sexyDiv :: Sexy -> Sexy -> Sexy
 sexyDiv (Norm (SexyInteger arg0)) (Norm (SexyInteger arg1)) =
   (Norm . SexyInteger) (div arg0 arg1)
 sexyDiv arg0 arg1 =
-  (Norm . SexyError) $ "(Error: Failed at division of " ++ show arg0 ++ " by " ++ show arg1 ++ ".)"
+  (Norm . SexyError) $ "(Error: failed at division of " ++ show arg0 ++ " by " ++ show arg1 ++ ")"
 
 sexyMod :: Sexy -> Sexy -> Sexy
 sexyMod (Norm (SexyInteger arg0)) (Norm (SexyInteger arg1)) =
   (Norm . SexyInteger) (mod arg0 arg1)
 sexyMod arg0 arg1 =
-  (Norm . SexyError) $ "(Error: Failed to yield the remainder from division of " ++ show arg0 ++ " by " ++ show arg1 ++ ".)"
+  (Norm . SexyError) $ "(Error: failed to yield the remainder from division of " ++ show arg0 ++ " by " ++ show arg1 ++ ")"
 
 sexyUnkownFunc :: Sexy -> Sexy -> Sexy
 sexyUnkownFunc arg0 arg1 =
-  (Norm . SexyError) $ "(Error: Unknown function applied to " ++ show arg0 ++ " and " ++ show arg1 ++ ".)"
+  (Norm . SexyError) $ "(Error: Unknown function applied to " ++ show arg0 ++ " and " ++ show arg1 ++ ")"
 
 showSexyFunc :: SexyFunc -> String
 showSexyFunc (Arthmetic ("+", _)) = "(Arthmetic: addition)"
@@ -76,36 +86,30 @@ showValue (SexyError err)       = "(SexyError: " ++ err ++ ")"
 
 instance Show Value where show = showValue
 
-parseSexy :: Parser Sexy
+parseSexy :: PCP.Parser Sexy
 parseSexy = -- spaces >> (node <|> parsed)
-  spaces >> (parseSexyNonNorm <|> parseSexyNorm)
+  PCP.spaces >> (parseSexyNonNorm PCP.<|> parseSexyNorm)
 
-parseSexyNonNorm :: Parser Sexy
+parseSexyNonNorm :: PCP.Parser Sexy
 parseSexyNonNorm = do
-  _ <- char '('
-  funcKey <- spaces >> manyTill anyToken (Text.ParserCombinators.Parsec.try space)
-  sexy1:_ <- spaces >> manyTill parseSexy
-    ((Text.ParserCombinators.Parsec.try space) <|> (Text.ParserCombinators.Parsec.try (char ')')))
-  sexy2:[] <- spaces >> manyTill parseSexy
-    ((Text.ParserCombinators.Parsec.try space) <|> (char ')'))
+  _ <- PCP.char '('
+  funcKey <- PCP.spaces >> PCP.manyTill PCP.anyToken (PCP.try PCP.space)
+  sexy1:_ <- PCP.spaces >> PCP.manyTill parseSexy
+    ((PCP.try PCP.space) PCP.<|> (PCP.try (PCP.char ')')))
+  sexy2:[] <- PCP.spaces >> PCP.manyTill parseSexy
+    ((PCP.try PCP.space) PCP.<|> (PCP.char ')'))
 
   let sexyFunc = case lookup funcKey sexyFuncs of
                   Just funcKeyhole -> (funcKey, funcKeyhole)
                   Nothing          -> (funcKey, sexyUnkownFunc)
-  return $ NonNorm (Arthmetic sexyFunc, sexy1, sexy2)
+    in
+    return $ NonNorm (Arthmetic sexyFunc, sexy1, sexy2)
 
-parseSexyNorm :: Parser Sexy
+parseSexyNorm :: PCP.Parser Sexy
 parseSexyNorm = do
-  atom <- many1 (digit <|> letter)
+  atom <- PCP.many1 (PCP.digit PCP.<|> PCP.letter)
 
   return $ Norm (SexyInteger (read atom))
-
-sexyFuncs :: [(String, Sexy -> Sexy -> Sexy)]
-sexyFuncs = [("+", sexyAdd),
-             ("-", sexySub),
-             ("*", sexyMulti),
-             ("/", sexyDiv),
-             ("%", sexyMod)]
 
 evalSexyFunc :: SexyFunc -> (Sexy -> Sexy -> Sexy)
 evalSexyFunc (Arthmetic (_, sexyFunc)) = sexyFunc
@@ -119,27 +123,28 @@ evalSexy sexy =
     val@(Norm (SexyError _))         -> val
 
 
-flushStr :: String -> IO ()
-flushStr str = putStr str >> hFlush stdout
+-- flushStr :: String -> IO ()
+-- flushStr str = putStr str >> IO.hFlush IO.stdout
 
-repl :: InputT IO ()
+repl :: HLine.InputT IO ()
 repl = do
-  maybeInput <- getInputLine "(SexyEval: )>>> "
-  let input = case maybeInput of
-       Just theInput -> theInput
-       Nothing       -> "Error: failed getting input" in
-    outputStrLn $ show
+  maybeInput <- HLine.getInputLine "(SexyEval: )>>> "
+  let input = "Error: failed getting input" `M.fromMaybe` maybeInput
+       -- case maybeInput of
+       -- Just theInput -> theInput
+       -- Nothing       -> "Error: failed getting input"
+    in
+    HLine.outputStrLn $ show
     $ evalSexy
-    $ case (parse parseSexy "" input) of
-    Right sexy -> sexy
-    Left err   -> (Norm . SexyError) (show err)
+    $ E.either (Norm . SexyError . show) id $ P.parse parseSexy "" input
+      -- case (parse parseSexy "" input) of
+      --   Right sexy -> sexy
+      --   Left err   -> (Norm . SexyError) (show err)
   repl
-
-
 
 main :: IO ()
 main = do
-  runInputT defaultSettings repl
+  HLine.runInputT HLine.defaultSettings repl
 --           args <- getArgs
 --           putStrLn $ head args
 --           pPrint (parse parseSexy "" (head args))
